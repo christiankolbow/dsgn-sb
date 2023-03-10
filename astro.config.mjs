36a220fefc5daeccf56c3ef1aee48f5b4e0dad54
christiankolbow <chris54833@aol.com>
2023-03-10 18:47:28 +0100

    add sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,9 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
-    image(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
   ],
   /*   vite: {
     plugins: [basicSsl()],
